fcd92a04b47369b995756b59255ba31425b9cdfa
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-01-20 20:46:16 +0200

    fixed

--- src/components/PokemonCardsContainer/PokemonCardsContainerStyles.js
+++ src/components/PokemonCardsContainer/PokemonCardsContainerStyles.js
@@ -5,6 +5,7 @@ export const PokemonCardsContainerStyles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         width: '70%',
+        marginBottom: 20
     },
     loadMoreBtn: {
         width: '100%',
@@ -12,7 +13,7 @@ export const PokemonCardsContainerStyles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 1,
         marginTop: 10,
-        marginBottom: 20
+        marginBottom: 2
     },
     loadMoreText: {
         textAlign: 'center',
